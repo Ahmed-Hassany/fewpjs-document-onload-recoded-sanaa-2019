6c44cc157fc6375817b90914f061ff820e6f13be
Ahmed Hassan <ahmedalhassany1@gmail.com>
2019-11-03 12:03:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 document.addEventListener("DOMContentLoaded", function(){
   
-let text = document.getElementById(text);
+let text = document.getElementById("text");
 text.textContent="This is really cool!";
 });
